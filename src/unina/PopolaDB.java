aa7be8375b4df5e71ae22dacb449e8e351fa3a82
Dacuj <davidecucciardi@gmail.com>
2026-06-08 11:28:43 +0200

    ordine cartelle2

---
+++ src/unina/PopolaDB.java
@@ -0,0 +1,13 @@
+package unina;
+
+import entity.*;
+public class PopolaDB {//classe per riempire il DB
+
+    private PopolaDB(){}
+
+    public static void PopolaDB(CatalogoCorsi catcorsi, CatalogoUtenti catutenti, Corso corso, Sezione sez){
+
+
+
+    return;}
+}
